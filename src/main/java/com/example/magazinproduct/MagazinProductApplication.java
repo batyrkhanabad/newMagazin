a8c3515f652994265e82bb0dfb9ea3a2585916c7
batyr_abad <batyrkhanabad@gmail.com>
2024-04-06 12:58:15 +0500

    Demo

--- src/main/java/com/example/magazinproduct/MagazinProductApplication.java
+++ src/main/java/com/example/magazinproduct/MagazinProductApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MagazinProductApplication {
+	///new line
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(MagazinProductApplication.class, args);
+		SpringApplication.run(MagazinProductApplication.class, args);//new class
 	}
-
 }
